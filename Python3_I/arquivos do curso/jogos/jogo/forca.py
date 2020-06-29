4ebbe80b5fd21f2960e9573498092bf125426f10
Brenda Maria <mariabrenda177@gmail.com>
2020-06-29 20:41:44 -0300

    Adicionando curso Python3_I

---
+++ Python3_I/arquivos do curso/jogos/jogo/forca.py
@@ -0,0 +1,8 @@
+
+def jogar():
+    print("********************************")
+    print("Bem vindo ao jofo de adivinhção!")
+    print("********************************")
+
+
+    print("Fim de jogo")
