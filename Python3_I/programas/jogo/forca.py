bb355607d67fa0b408d6593205a7551d593039e1
Brenda Maria <mariabrenda177@gmail.com>
2020-07-06 23:43:43 -0300

    adicionando jogo da forca

--- Python3_I/programas/jogo/forca.py
+++ Python3_I/programas/jogo/forca.py
@@ -4,5 +4,24 @@ def jogar():
     print("Bem vindo ao jofo de adivinhção!")
     print("********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+        print("jogando...")
+
 
     print("Fim de jogo")
+
+if(__name__ == "__main__"):
+    jogar()
