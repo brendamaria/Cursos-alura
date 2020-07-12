7137fa26f7be57ee43b47dac8aaf0f17dc43acec
Brenda Maria <mariabrenda177@gmail.com>
2020-07-12 16:51:48 -0300

    arruamando jogo da forca

--- Python3_I/programas/jogo/forca.py
+++ Python3_I/programas/jogo/forca.py
@@ -3,15 +3,10 @@ def jogar():
     print("Bem vindo ao jofo de adivinhção!")
     print("********************************")
 
-<<<<<<< HEAD
-    palavra_secreta = "banana"
-    letras_acertadas = ["_","_","_","_","_","_"]
-=======
     palavra_secreta = "maça".upper()
     letras_acertadas = ["_" for letra in palavra_secreta]
 
     print(letras_acertadas)
->>>>>>> 3d577246b6cf313925d70dfbc8dfe1d395e97721
 
     enforcou = False
     acertou = False
@@ -19,18 +14,6 @@ def jogar():
 
     while (not enforcou and not acertou):
         chute = input("Qual letra? ")
-<<<<<<< HEAD
-        chute = chute.strip()
-
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
-        print(letras_acertadas)
-
-
-=======
         chute = chute.strip().upper()
 
         if (chute in palavra_secreta):
@@ -50,7 +33,6 @@ def jogar():
         print("Você ganhou!")
     else:
         print("Você perdeu!")
->>>>>>> 3d577246b6cf313925d70dfbc8dfe1d395e97721
     print("Fim de jogo")
 
 
