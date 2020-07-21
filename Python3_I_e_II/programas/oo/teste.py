b466a8eadd7a8a3203088eebb41c00aee489a847
Brenda Maria <mariabrenda177@gmail.com>
2020-07-20 21:55:30 -0300

    python primeiro cap

--- Python3_I_e_II/programas/oo/teste.py
+++ Python3_I_e_II/programas/oo/teste.py
@@ -1,1 +1,14 @@
-def cria_conta("numero": numero, "titular": titular, "saldo": saldo, "limite": limite):
+def cria_conta(numero, titular, saldo, limite):
+    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
+    return conta
+
+def deposita(conta, valor):
+    conta["saldo"] += valor
+
+def saca(conta, valor):
+    conta["saldo"] -= valor
+
+def extrato(conta):
+    print("saldo {}".format(conta["saldo"]))
+
+
