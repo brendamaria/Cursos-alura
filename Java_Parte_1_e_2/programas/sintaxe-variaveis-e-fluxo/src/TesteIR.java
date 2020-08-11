c5457e103278c0d6707453634b8ac87d91c06b19
Brenda Maria <mariabrenda177@gmail.com>
2020-08-11 17:08:53 -0300

    teste de IR

--- Java_Parte_1_e_2/programas/sintaxe-variaveis-e-fluxo/src/TesteIR.java
+++ Java_Parte_1_e_2/programas/sintaxe-variaveis-e-fluxo/src/TesteIR.java
@@ -3,9 +3,6 @@ public class TesteIR {
 	public static void main(String[] args) {
 
         double salario = 3300.0;
-        //De 1900.0 até 2800.0, o IR é de 7.5% e 
-        //pode deduzir na declaração o valor de R$ 142
-
 
         if (salario >= 1900.0 && salario <= 2800.0) {
         	System.out.println("a sua aliquota é de 7.5%");
