e68ddebc734133fe50a292fe0e51cf17b9344f6b
brendaMac <mariabrenda177@gmail.com>
2020-09-12 20:03:01 -0300

    deletando pasta antiga de java e python

--- Java_Parte_1_e_2/programas/byte-bank-herdado/src/Designer.java
+++
@@ -1,8 +0,0 @@
-
-public class Designer extends Funcionario {
-	
-	public double getBonificacao() {
-		System.out.println("chamando bonificacao do designer");
-		return super.getBonificacao() + 150;
-	}
-}
