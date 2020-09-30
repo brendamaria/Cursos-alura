e5fdafc34665cbfab7d6aa41dbab67b1a2753e61
brenad maria <mariabrenda177@gmail.com>
2020-09-30 15:00:30 -0300

    gitignore

--- Java/programas/java-pilha/src/FluxoComTratamento.java
+++ Java/programas/java-pilha/src/FluxoComTratamento.java
@@ -5,7 +5,7 @@ public class FluxoComTratamento {
         System.out.println("Ini do main");
         try {
         	metodo1();      
-        } catch (ArithmeticException | NullPointerException ex) {
+        } catch (ArithmeticException | NullPointerException | MinhaExcecao ex) {
         	String msg = ex.getMessage();
             System.out.println("Exception " + msg);
             ex.printStackTrace();
@@ -13,15 +13,15 @@ public class FluxoComTratamento {
         System.out.println("Fim do main");
     }
 
-    private static void metodo1() {
+    private static void metodo1() throws MinhaExcecao {
         System.out.println("Ini do metodo1");
         metodo2();
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() {
+    private static void metodo2() throws MinhaExcecao {
         System.out.println("Ini do metodo2");
-        throw new ArithmeticException("Deu ruim");
+        throw new MinhaExcecao("Deu ruim");
     }
 	
 }
